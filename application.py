880f077259d4ed83c751b31a1dbc52051a36ea11
KNU753N <knutsen.b@gmail.com>
2022-01-06 23:40:02 +0100

    added age calculator

--- application.py
+++ application.py
@@ -1,13 +1,19 @@
 import os
 from flask import Flask, request, redirect, render_template
+from datetime import date
 
 navbar = os.listdir('/home/bjrnktip/mywebapp/templates')
 
 app = Flask(__name__)
 
+def alder(fdag):
+    idag = date.today()
+    alder = idag.year - fdag.year - ((idag.month, idag.day) < (fdag.month, fdag.day))
+    return alder
+
 @app.route("/")
 def index():
-    return render_template("index.html", navbar=navbar)
+    return render_template("index.html", navbar=navbar, alder = alder(date(1990,7,31)))
 
 @app.route("/om")
 def om():
